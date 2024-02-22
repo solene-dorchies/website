2b9523fb194d556de3982ec8b925631e38fff105
WORK <dorchies.c@gmail.com>
2024-02-22 19:40:50 +0100

    Img + footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
           <img src="./../img/Logo-SD.png" alt="" className="Logo" />
         </div>
         <div>
-          <p>Solène Dorchies - © 2024 v.1</p>
+          <p>Solène Dorchies - © 2024</p>
         </div>
       </div>
     </div>
