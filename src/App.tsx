66f09b46fe3d4ea8d0fa6f9391120424604f44df
WORK <dorchies.c@gmail.com>
2024-02-22 15:42:15 +0100

    Correction

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ export default function App() {
       <Header />
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/website" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/error-page-not-found" element={<NotFound />} />
